e18fe5ce0abe2df168ce7a0b783259a112059a34
Scott Jones <scott.jones4k@gmail.com>
2020-10-07 14:27:24 +0100

    Added web api to generate id's over the network

---
+++ IdentityGeneration.WebApi/Controllers/GenerateController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IdentityGeneration.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GenerateController : ControllerBase
+    {
+        private readonly IIdGenerator _idGenerator;
+
+        public GenerateController(IIdGenerator idGenerator)
+        {
+            _idGenerator = idGenerator;
+        }
+
+        [HttpPost]
+        public long Post() => _idGenerator.Generate();
+    }
+}
